8ad33b67b33b21b39631b0e6bc5921d954421aaf
LevinsonEli <eliyahulevinson@gmail.com>
2018-08-23 00:15:30 +0200

    BarleyBreak v1.0(Refactored)

--- script.js
+++ script.js
@@ -1,67 +1,38 @@
 
 var size = 4;	// Size of the game table (standart: 4)
-var arr = new Array();	// Game array
+var gameArr = new Array();	// Game array
 
 var stepsCount = 0;
 var nowDate;
 
-
 var newGameBtn = document.querySelector(".newGameBtn");
-
 var recordTable = document.querySelector(".recordTbody");
-
 var gameBox = document.querySelector("#gameBox");
-var newTable = document.createElement("table");
-	newTable.className = "newTable";
+var gameTable = document.createElement("table");
+	gameTable.className = "gameTable";
 
-// Record Table
+// Record Table by default
 var arrRecords = [	
-	{
-		nick:  "Solt",
-		time: 54.6
-	},
-	{
-		nick:  "Seed",
-		time: 123.2
-	},
-	{
-		nick:  "Ela",
-		time: 24.7
-	},
-	{
-		nick:  "Symon",
-		time: 86.4
-	},
-	{
-		nick:  "Nesty",
-		time: 44.5
-	},
-	{
-		nick:  "Cola",
-		time: 75.2
-	},
-	{
-		nick:  "Sendy",
-		time: 78.0
-	},
-	{
-		nick:  "Tomb",
-		time: 35.2
-	},
-	{
-		nick:  "Ferron",
-		time: 156.1
-	}
+	{ nick:  "Solt", time: 54.6	},
+	{ nick:  "Seed", time: 123.2 },
+	{ nick:  "Ela", time: 24.7 },
+	{ nick:  "Symon", time: 86.4 },
+	{ nick:  "Nesty", time: 44.5 },
+	{ nick:  "Cola", time: 75.2 },
+	{ nick:  "Sendy", time: 78.0 },
+	{ nick:  "Tomb", time: 35.2 },
+	{ nick:  "Ferron", time: 156.1 }
 ];
 
-for ( var i = 0; i < (size*size); i++) 	// Fulling the array by numbers
-	arr[i] = i + 1;
+for ( var i = 0; i < (size*size); i++) 	// Filling the array by numbers
+	gameArr[i] = i + 1;
 
 newGame = function(){
-	arr.sort(function(a, b){ return Math.random() - 0.5; }); // Sorting the array randomly	
-	
-	//Creating a table of numbers
-	newTable.innerHTML = "";
+	stepsCount = 0;
+	gameArr.sort( (a, b) => (Math.random() - 0.5) ); // Sorting the array randomly	
+
+	//Creating a HTML table of numbers
+	gameTable.innerHTML = "";
 	for(var i = 0; i < size; i++)
 	{
 		var newTR = document.createElement("tr");
@@ -70,16 +41,16 @@ newGame = function(){
 			var newTD = document.createElement("td");
 			newTD.id = (i * size + j).toString();
 
-			if (arr[i * size + j] == size * size)
+			if (gameArr[i * size + j] == size * size)
 				newTD.innerHTML += " ";
 			else
-				newTD.innerHTML += arr[i * size + j].toString();
+				newTD.innerHTML += gameArr[i * size + j].toString();
 			newTR.appendChild(newTD);
 		}
-		newTable.appendChild(newTR);
+		gameTable.appendChild(newTR);
 	}
 	gameBox.innerHTML = "";
-	gameBox.appendChild(newTable);
+	gameBox.appendChild(gameTable);
 }
 
 printRecordTable = function(){
@@ -99,78 +70,69 @@ recreateRocordTable = function(newRecord){
 }
 
 // Function that checks if the game is ended (completed)
-function gameIsCompleted(arr, size)
+function gameIsCompleted(gameArr, size)
 {
 	for ( var i = 0; i < size * size; i++)
-		if (arr[i] != i + 1)
+		if (gameArr[i] != i + 1)
 			return false;
-
 	return true;
 }
 
 // Function that ckecks if the cell can be moved and if it can - returns the vector (string)
 // another way returns false (string)
-function canBeMoved(arrThis, size, indexThis)
+function canBeMoved(gameArr, size, indexThis)
 {
-	if ( indexThis >= size && arrThis[indexThis - size] == size*size )	// NOT The First Line
+	if ( indexThis >= size && gameArr[indexThis - size] == size*size )	// NOT The First Line
 		return "up";
-	else if ( (indexThis + size) < (size * size) && arrThis[indexThis + size] == size*size )
+	else if ( (indexThis + size) < (size * size) && gameArr[indexThis + size] == size*size )
 		return "down";
-	else if ( (indexThis + 1) % size != 1 && arrThis[indexThis - 1] == size*size )
+	else if ( (indexThis + 1) % size != 1 && gameArr[indexThis - 1] == size*size )
 		return  "left";
-	else if ( (indexThis + 1) % size != 0 && arrThis[indexThis + 1] == size*size )
+	else if ( (indexThis + 1) % size != 0 && gameArr[indexThis + 1] == size*size )
 		return  "right";
 	return "false";
 }
 
 // The function that moving a cell. It changing the array and the table.
-// The function get array, it's sqrt.size, index of the cell that need to move and the vector of moving
-// And it returns changed array.
-function moveBox(arrThis, size, indexThis, vector)
+// The function gets an array, the size of the game, index of the cell that need to move and the vector of moving
+// And returns changed array.
+function moveCellInTable(gameArr, size, indexThis, vector)
 {
-	var arrTd = document.querySelectorAll("table.newTable td");
+	var arrTd = document.querySelectorAll(".gameTable td");
 	switch(vector)
 	{
 		case "up":
-			arrTd[indexThis - size].innerHTML = arrTd[indexThis].innerHTML;
-			arrTd[indexThis].innerHTML = " ";
-			
-			arrThis[indexThis - size] = arrThis[indexThis];
-			arrThis[indexThis] = size*size;
+			gameArr = moveCellInTwoCells(gameArr, size, arrTd, indexThis, indexThis - size);
 			break;
 		case "down":
-			arrTd[indexThis + size].innerHTML = arrTd[indexThis].innerHTML;
-			arrTd[indexThis].innerHTML = " ";
-			
-			arrThis[indexThis + size] = arrThis[indexThis];
-			arrThis[indexThis] = size*size;
+			gameArr = moveCellInTwoCells(gameArr, size, arrTd, indexThis, indexThis + size);
 			break;
 		case "left":
-			arrTd[indexThis - 1].innerHTML = arrTd[indexThis].innerHTML;
-			arrTd[indexThis].innerHTML = " ";
-			
-			arrThis[indexThis - 1] = arrThis[indexThis];
-			arrThis[indexThis] = size*size;
+			gameArr = moveCellInTwoCells(gameArr, size, arrTd, indexThis, indexThis - 1);
 			break;
 		case "right":
-			arrTd[indexThis + 1].innerHTML = arrTd[indexThis].innerHTML;
-			arrTd[indexThis].innerHTML = " ";
-			
-			arrThis[indexThis + 1] = arrThis[indexThis];
-			arrThis[indexThis] = size*size;
+			gameArr = moveCellInTwoCells(gameArr, size, arrTd, indexThis, indexThis + 1);
 			break;
-			
 		default: break;
 	}
-	return arrThis;
+	return gameArr;
 }
 
+function moveCellInTwoCells(gameArr, size, arrTd, indexThis, indexNext)
+{
+	arrTd[indexNext].innerHTML = arrTd[indexThis].innerHTML;
+	arrTd[indexThis].innerHTML = " ";
+			
+	gameArr[indexNext] = gameArr[indexThis];
+	gameArr[indexThis] = size*size;
+	return gameArr;
+}
 
 newGame();
 recreateRocordTable();
 printRecordTable();
 
-var Table = document.querySelector(".newTable");
+var Table = document.querySelector(".gameTable");
 Table.onclick = function(event)
 {
 	if ( stepsCount == 0 )
@@ -180,23 +142,20 @@ Table.onclick = function(event)
 	
 	if (el.id != "")
 	{
-		arr = moveBox(arr, size, +el.id, canBeMoved(arr, size, +el.id));
+		gameArr = moveCellInTable(gameArr, size, +el.id, canBeMoved(gameArr, size, +el.id));
 		stepsCount++;
 	}
-	if (gameIsCompleted(arr, size))
+	if (gameIsCompleted(gameArr, size))
 	{
 		var endDate = new Date();
 		var name = prompt("Your nickname? ", "NickName");
 
 		recreateRocordTable({'nick': name, 'time': ((endDate - nowDate) / 1000).toFixed(1)});
 		printRecordTable();
-		
-		stepsCount = 0;
 		newGame();
 	}
 }
 
 newGameBtn.addEventListener("click", function(){
-	stepsCount = 0;
 	newGame();
 }, false);
